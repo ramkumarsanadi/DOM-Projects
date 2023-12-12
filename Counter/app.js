190442cfc6e0dae85dd9f41efd70b078c4af7802
RAMKUMAR S <ramkumarsanadi1@gmail.com>
2023-12-12 12:13:56 +0530

    added counter project and reviews project

---
+++ Counter/app.js
@@ -0,0 +1,35 @@
+    const value = document.getElementById('value')
+    const btns = document.querySelectorAll('.btn')
+
+    let count = 0
+
+    btns.forEach(btn => {
+
+        btn.addEventListener('click', (e)=> {
+            const styles = e.currentTarget.classList
+
+            if(styles.contains('Increase')){
+                count += 1
+  
+            }else if(styles.contains('decrease')){
+                count -=1
+            
+            }else {
+                count = 0
+                
+            }
+            if(count < 0){
+                value.style.color = 'red'
+            }
+            else if(count > 0){
+                value.style.color = 'green'
+            }
+            else {
+                value.style.color = 'black'
+            }
+         
+            value.textContent = count
+        })
+        
+    });
+   
